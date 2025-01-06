6680cd10d54afd29ebb7d00e18201a16fb95921d
harisaltafb105 <haris_memon1@yahoo.com>
2025-01-06 21:47:17 +0500

    Second Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,28 @@
-import Image from "next/image";
+import React from 'react'
+import { urlFor } from '@/sanity/lib/image'
 
-export default function Home() {
-  return (<div>
-hello from me
-  </div>)
+import { client } from '@/sanity/lib/client'
+import Image from 'next/image'
+
+ const  Homepage = async() => {
+     const query = `*[_type == 'blog']  `
+
+const data:Blog[] = await client.fetch(query)
+
+
+  return (
+    <div  >
+       
+        {data.map((info)=>(
+          <div key={info.slug}>
+            <h1>{info.Title}</h1>
+            <p>{info.Paragraph}</p>
+            
+            </div>
+        ))}    
+
+    </div>
+  )
 }
+
+export default Homepage
---
+++ src/app/type.ts
@@ -0,0 +1,8 @@
+type Blog ={
+    Title: string,
+    Paragraph: string,
+    image: any,
+    slug: string,
+    block:String
+
+}
